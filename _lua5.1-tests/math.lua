d3de08565a5879fd25968dfbcce8d7082b85edcf
Yahia Sweid <yhya.swid@foodpanda.com>
2024-06-08 19:39:22 +0800

    Fix parsing exp number

--- _lua5.1-tests/math.lua
+++ _lua5.1-tests/math.lua
@@ -52,6 +52,7 @@ assert(tonumber(string.rep('1', 32), 2) + 1 == 2^32)
 assert(tonumber('-fffffFFFFF', 16)-1 == -2^40)
 assert(tonumber('ffffFFFF', 16)+1 == 2^32)
 assert(tonumber('0xF') == 15)
+assert(tonumber('1e-06') == 0.000001)
 
 assert(1.1 == 1.+.1)
 print(100.0, 1E2, .01)
